c49459325510030b04e94e80d844d37ac6df8c16
Ryan30012 <74203166+Ryan30012@users.noreply.github.com>
2025-09-04 02:35:06 -0400

    Finishing displaying the UI main menu

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,43 @@ bool HandlingUIMenu() {
     sf::RenderWindow ui_menu(sf::VideoMode({800u, 800u}), "Chess Game");
     ui_menu.setFramerateLimit(144);
 
+    // Adding a Title
+    const sf::Font font("../../fonts/Inter_18pt-Regular.ttf");
+    sf::Text title(font, "CHESS GAME");
+    title.setCharacterSize(60);
+    title.setPosition({204,152});
+    title.setFillColor(sf::Color::Black);
+    
+    // Adding Button Player vs Player named btn1
+    sf::RectangleShape btn1_rect;
+    btn1_rect.setSize(sf::Vector2f(381,104));
+    btn1_rect.setPosition({209,306});
+    btn1_rect.setFillColor(sf::Color(217,217,217,255));
+    sf::Text btn1_txt(font, "Player VS Player");
+    btn1_txt.setCharacterSize(38);
+    btn1_txt.setPosition({252,335});
+    btn1_txt.setFillColor(sf::Color::Black);
+
+    // Adding Button Player vs AI named btn2
+    sf::RectangleShape btn2_rect;
+    btn2_rect.setSize(sf::Vector2f(381,104));
+    btn2_rect.setPosition({209,452});
+    btn2_rect.setFillColor(sf::Color(217,217,217,255));
+    sf::Text btn2_txt(font, "Player VS    AI");
+    btn2_txt.setCharacterSize(38);
+    btn2_txt.setPosition({252,481});
+    btn2_txt.setFillColor(sf::Color::Black);
+
+    // Adding Button Exit named btnE
+    sf::RectangleShape btnE_rect;
+    btnE_rect.setSize(sf::Vector2f(381,104));
+    btnE_rect.setPosition({209,598});
+    btnE_rect.setFillColor(sf::Color(217,217,217,255));
+    sf::Text btnE_txt(font, "Exit");
+    btnE_txt.setCharacterSize(38);
+    btnE_txt.setPosition({359,627});
+    btnE_txt.setFillColor(sf::Color::Black);
+
 
     while (ui_menu.isOpen())
     {
@@ -27,7 +64,18 @@ bool HandlingUIMenu() {
             }
         }
         
-        ui_menu.clear(sf::Color::Black);
+        ui_menu.clear(sf::Color::White);
+        // Drawing Buttons and Texts
+        ui_menu.draw(title);
+        ui_menu.draw(btn1_rect);
+        ui_menu.draw(btn1_txt);
+
+        ui_menu.draw(btn2_rect);
+        ui_menu.draw(btn2_txt);
+
+        ui_menu.draw(btnE_rect);
+        ui_menu.draw(btnE_txt);
+
         ui_menu.display();
     }
 
@@ -41,12 +89,6 @@ int main()
     if (!HandlingUIMenu()) {
         return 1;
     }
-
-    std::cout << "UI Menu closed" << std::endl;
-
-    int val;
-
-    std::cin >>  val;
 
     /*
     sf::RenderWindow window(sf::VideoMode({800u, 800u}), "Chess Game");
