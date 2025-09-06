d6e9299a66ed07b2cc9785925ab03c7bc0b9de25
Ryan30012 <74203166+Ryan30012@users.noreply.github.com>
2025-09-05 23:56:05 -0400

    Creating Constant file

---
+++ src/ChessConstant.hpp
@@ -0,0 +1,27 @@
+#ifndef CHESSCONSTANT_HPP
+#define CHESSCONSTANT_HPP
+
+#include <SFML/Window.hpp>
+
+namespace ChessConst {
+
+    sf::Color lightTile = sf::Color(221, 207, 130, 255);
+    sf::Color darkTile = sf::Color(185, 144, 68, 255);
+    sf::Color activeTile = sf::Color(0, 81, 255, 85);
+    sf::Color moveTile = sf::Color(0, 255, 30, 135);
+    sf::Color checkTile = sf::Color(255, 0, 0, 115);
+
+    enum TileType {
+        DEFAULT,
+        ACTIVE,
+        MOVE,
+        CHECK
+    }
+
+    const float WINDOW_SIZE = 800f; 
+
+}
+
+
+
+#endif
